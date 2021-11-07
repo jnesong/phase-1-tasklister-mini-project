c4ddf4b76139f416ff7325d2742deeacd2697b08
Jenny Chau <jnechau@gmail.com>
2021-11-07 17:13:42 -0500

    added delete button

--- src/index.js
+++ src/index.js
@@ -4,13 +4,25 @@ document.addEventListener("DOMContentLoaded", () => {
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     buildToDoList(e.target.new_task_description.value);
+    form.reset()
   });
     
-function buildToDoList (toDoPara){
+const buildToDoList = (toDoPara) => {
+  //add items to build list
   let taskList = document.createElement('li');
-  taskList.textContent = toDoPara;
+  taskList.textContent = toDoPara + " ";
   document.querySelector("#tasks").appendChild(taskList)
-}
+  console.log(taskList);
+  //add delete button
+  let delButton = document.createElement("button");
+  delButton.textContent = "delete this";
+  taskList.appendChild(delButton);
+  delButton.addEventListener("click", delItem);
 
+}
+//function for deleting that is pasesed through the button's event listener
+const delItem = (e) => {
+  e.target.parentNode.remove()
+}
 
 });
