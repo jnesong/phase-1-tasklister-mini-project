9e2e42c96238899246281f79a253f4d19c6c6e36
Jenny Chau <jnechau@gmail.com>
2021-11-07 16:42:17 -0500

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const form = document.querySelector("#create-task-form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    buildToDoList(e.target.new_task_description.value);
+  });
+    
+function buildToDoList (toDoPara){
+  let taskList = document.createElement('li');
+  taskList.textContent = toDoPara;
+  document.querySelector("#tasks").appendChild(taskList)
+}
+
+
 });
